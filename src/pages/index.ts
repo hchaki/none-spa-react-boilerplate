9d61de08b2f20290b876904b1686ce3e682c6b09
chaki <hchaki@users.noreply.github.com>
2020-07-02 14:23:35 +0900

    Initial commit

---
+++ src/pages/index.ts
@@ -0,0 +1,22 @@
+import './style.scss';
+import * as test from '@ts/sample/test';
+import { commonApi } from '@ts/utils/API/config';
+import { endPoints } from '@ts/utils/API/endpoints';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const testGetButton = document.getElementById('testGet');
+
+  testGetButton?.addEventListener('click', async () => {
+    try {
+      const { data } = await commonApi.get(endPoints.testArray);
+      alert(JSON.stringify(data));
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  const testAnotherFileButton = document.getElementById('testJs');
+  testAnotherFileButton?.addEventListener('click', async () => {
+    test.testAlert(test.testString);
+  });
+});
